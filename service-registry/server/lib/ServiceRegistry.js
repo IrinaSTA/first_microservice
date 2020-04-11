9a297b8bb82718676fbc9b7db29fb8acaba9afbd
Irina Baldwin <ibaldwin@deloitte.co.uk>
2020-04-11 16:24:14 +0100

    Adds cleanup

--- service-registry/server/lib/ServiceRegistry.js
+++ service-registry/server/lib/ServiceRegistry.js
@@ -8,6 +8,9 @@ class ServiceRegistry {
   }
 
   get(name, version) {
+    console.log(this.services);
+    this.cleanup();
+    console.log(this.services);
     const candidates = Object.values(this.services)
       .filter(service => service.name === name && semver.satisfies(service.version, version));
 
@@ -15,18 +18,19 @@ class ServiceRegistry {
   }
 
   register(name, version, ip, port) {
+    this.cleanup();
     const key = name + version + ip + port;
 
     if (!this.services[key]) {
       this.services[key] = {};
-      this.services[key].timestamp = Math.floor(new Date () / 1000);
+      this.services[key].timestamp = Math.floor(new Date() / 1000);
       this.services[key].ip = ip;
       this.services[key].name = name;
       this.services[key].version = version;
       this.log.debug(`Added service ${name}, version ${version}, listening at ${ip}:${port}`);
       return key;
     }
-    this.services[key].timestamp = Math.floor(new Date () / 1000);
+    this.services[key].timestamp = Math.floor(new Date() / 1000);
     this.log.debug(`Updated service ${name}, version ${version}, listening at ${ip}:${port}`);
     return key;
   }
@@ -35,6 +39,16 @@ class ServiceRegistry {
     const key = name + version + ip + port;
     delete this.services[key];
     return key;
+  }
+
+  cleanup() {
+    const now = Math.floor(new Date() / 1000);
+    Object.keys(this.services).forEach((key) => {
+      if (this.services[key].timestamp + this.timeout > now) {
+        delete this.services[key];
+        this.log.debug(`Removed service ${key}`);
+      }
+    });
   }
 }
 
